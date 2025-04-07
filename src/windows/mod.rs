619eafa2ec7525fb5eb8e0f9ae7a7373e19c65e8
Dirkjan Ochtman <dirkjan@ochtman.nl>
2025-04-07 15:23:12 +0200

    Switch to windows-bindgen

--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -5,11 +5,10 @@ use std::io;
 #[cfg(feature = "set")]
 use std::os::windows::ffi::OsStrExt;
 use std::os::windows::ffi::OsStringExt;
+use std::ptr;
 
-use windows::core::PWSTR;
-use windows::Win32::System::SystemInformation::{
-    ComputerNamePhysicalDnsHostname, GetComputerNameExW,
-};
+mod bindings;
+use bindings::{ComputerNamePhysicalDnsHostname, GetComputerNameExW, PWSTR};
 
 pub fn get() -> io::Result<OsString> {
     let mut size = 0;
@@ -17,8 +16,9 @@ pub fn get() -> io::Result<OsString> {
         // Don't care much about the result here,
         // it is guaranteed to return an error,
         // since we passed the NULL pointer as a buffer
-        let result = GetComputerNameExW(ComputerNamePhysicalDnsHostname, PWSTR::null(), &mut size);
-        debug_assert!(result.is_err());
+        let result =
+            GetComputerNameExW(ComputerNamePhysicalDnsHostname, ptr::null_mut(), &mut size);
+        debug_assert_eq!(result, 0);
     };
 
     let mut buffer = Vec::with_capacity(size as usize);
@@ -26,27 +26,26 @@ pub fn get() -> io::Result<OsString> {
     let result = unsafe {
         GetComputerNameExW(
             ComputerNamePhysicalDnsHostname,
-            PWSTR::from_raw(buffer.as_mut_ptr()),
+            PWSTR::from(buffer.as_mut_ptr()),
             &mut size,
         )
     };
 
     match result {
-        Ok(_) => {
+        0 => Err(io::Error::last_os_error()),
+        _ => {
             unsafe {
                 buffer.set_len(size as usize);
             }
 
             Ok(OsString::from_wide(&buffer))
         }
-        Err(e) => Err(io::Error::from_raw_os_error(e.code().0)),
     }
 }
 
 #[cfg(feature = "set")]
 pub fn set(hostname: &OsStr) -> io::Result<()> {
-    use windows::core::PCWSTR;
-    use windows::Win32::System::SystemInformation::SetComputerNameExW;
+    use bindings::{SetComputerNameExW, PCWSTR};
 
     let mut buffer = hostname.encode_wide().collect::<Vec<_>>();
     buffer.push(0x00); // Appending the null terminator
@@ -54,9 +53,12 @@ pub fn set(hostname: &OsStr) -> io::Result<()> {
     let result = unsafe {
         SetComputerNameExW(
             ComputerNamePhysicalDnsHostname,
-            PCWSTR::from_raw(buffer.as_ptr()),
+            PCWSTR::from(buffer.as_ptr()),
         )
     };
 
-    result.map_err(|e| io::Error::from_raw_os_error(e.code().0))
+    match result {
+        0 => Err(io::Error::last_os_error()),
+        _ => Ok(()),
+    }
 }
